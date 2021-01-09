4b63d066942a197d0d71aaf4f790b45264671c1d
Myrmex <dfusi@hotmail.com>
2021-01-09 10:49:29 +0100

    Work authors completed

--- projects/myrmidon/cadmus-biblio-ui/src/lib/components/work-authors/work-authors.component.ts
+++ projects/myrmidon/cadmus-biblio-ui/src/lib/components/work-authors/work-authors.component.ts
@@ -287,7 +287,7 @@ export class WorkAuthorsComponent implements OnInit {
 
   public cancel(): void {
     this.editing = false;
-    this.current = this.buildCurrent();
+    this.updateForm(this._model);
     this.editorClose.emit();
   }
 
--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { WorkBase } from '@myrmidon/cadmus-biblio-core';
+import { WorkAuthor, WorkBase } from '@myrmidon/cadmus-biblio-core';
 import { ThesaurusEntry } from '@myrmidon/cadmus-core';
 
 @Component({
@@ -11,6 +11,7 @@ export class DemoComponent implements OnInit {
   public work: WorkBase | undefined;
   public container: WorkBase | undefined;
 
+  public authors: WorkAuthor[] | undefined;
   public roleEntries: ThesaurusEntry[];
 
   constructor() {
@@ -29,5 +30,9 @@ export class DemoComponent implements OnInit {
 
   public onContainerChange(container: WorkBase): void {
     this.container = container;
+  }
+
+  public onAuthorsChange(authors: WorkAuthor[]): void {
+    this.authors = authors;
   }
 }
