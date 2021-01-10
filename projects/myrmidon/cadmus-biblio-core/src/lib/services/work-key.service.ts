939754103e09395659489dc387b2ee86283caff5
Myrmex <dfusi@hotmail.com>
2021-01-10 16:44:41 +0100

    Key builder ordinals

--- projects/myrmidon/cadmus-biblio-core/src/lib/services/work-key.service.ts
+++ projects/myrmidon/cadmus-biblio-core/src/lib/services/work-key.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
-import { WorkBase } from '../models';
+import { WorkBase, WorkAuthor } from '../models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WorkKeyService {
+  /**
+   * Build the key for the specified work or container.
+   * @param work The work or container to build the key for.
+   * @returns The key.
+   */
   public buildKey(work: WorkBase): string {
     if (!work) {
       return '';
     }
     const sb: string[] = [];
     if (work.authors?.length) {
-      const lastNames = work.authors.map((a) => a.last);
-      lastNames.sort();
-      sb.push(lastNames.join(' & '));
+      const sorted = [...work.authors];
+      sorted.sort((a: WorkAuthor, b: WorkAuthor) => {
+        if (a.ordinal !== b.ordinal) {
+          return (a.ordinal || 0) - (b.ordinal || 0);
+        } else {
+          return a.last.localeCompare(b.last);
+        }
+      });
+      sb.push(sorted.map((a) => a.last).join(' & '));
     }
     sb.push(' ');
     sb.push((work.yearPub || 0).toString());
