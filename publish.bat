ddbfda30fd3e8c83ce23e557b00dc3f4010270b1
Myrmex <dfusi@hotmail.com>
2021-01-21 12:26:33 +0100

    Docker

---
+++ publish.bat
@@ -0,0 +1,19 @@
+@echo off
+echo NPM PUBLISH
+echo Before continuing, ensure that:
+echo - you are logged in (npm whoami)
+echo - you have successfully rebuilt all the libraries (npm run build-all)
+pause
+cd .\dist\myrmidon\cadmus-biblio-api
+call npm publish --access=public
+cd ..\..\..
+pause
+cd .\dist\myrmidon\cadmus-biblio-core
+call npm publish --access=public
+cd ..\..\..
+pause
+cd .\dist\myrmidon\cadmus-biblio-ui
+call npm publish --access=public
+cd ..\..\..
+pause
+echo ALL DONE
