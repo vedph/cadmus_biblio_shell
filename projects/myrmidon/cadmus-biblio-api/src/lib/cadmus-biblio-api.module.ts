d375bbe13597c668b189d95207e101ef4b5602fb
Myrmex <dfusi@hotmail.com>
2021-06-30 17:22:01 +0200

    EnvServiceProvider

--- projects/myrmidon/cadmus-biblio-api/src/lib/cadmus-biblio-api.module.ts
+++ projects/myrmidon/cadmus-biblio-api/src/lib/cadmus-biblio-api.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { EnvServiceProvider } from '@myrmidon/cadmus-core';
 
 @NgModule({
   declarations: [],
   imports: [],
   exports: [],
+  providers: [
+    EnvServiceProvider
+  ]
 })
 export class CadmusBiblioApiModule {}
